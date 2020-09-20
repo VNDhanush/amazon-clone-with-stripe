8b373f57f03089a80953cecf94194201368676b6
Dhanush <vndhanush51@gmail.com>
2020-09-20 15:28:31 +0530

    added firebase and removed test files form react

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCVcT2n4bbvIj7hCpEornobDJdQ0x_ciOg",
+    authDomain: "project-stripe.firebaseapp.com",
+    databaseURL: "https://project-stripe.firebaseio.com",
+    projectId: "project-stripe",
+    storageBucket: "project-stripe.appspot.com",
+    messagingSenderId: "1047505913177",
+    appId: "1:1047505913177:web:62417ba6038e40c93e3848",
+    measurementId: "G-LHTJE6VJ70"
+};
