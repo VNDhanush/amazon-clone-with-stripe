415e1fb7491ef45e9663aa7ff16789f9615ad60d
Dhanush <vndhanush51@gmail.com>
2020-09-23 16:07:29 +0530

    checkout page has been edited to make it look good for 0 item and more items.

--- src/Checkout.js
+++ src/Checkout.js
@@ -15,23 +15,28 @@ function Checkout() {
                     src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
                     alt=""
                 />
-            
-                <div><h3>Hey,{user?.email}</h3>
-                    <h2 className="checkout__title">
-                        Your Shopping Basket
+                {basket?.length === 0 ? (
+                    <div>
+                        <h2>Your Shopping Basket is empty</h2>
+                        <p>You have no items in your basket. To buy one or  more items,click "Add to basket" next to the item.</p>
+                    </div>
+                ) : (
+                        <div><h3>Hey,{user?.email}</h3>
+                            <h2 className="checkout__title">
+                                Your Shopping Basket
                 </h2>
-                    {basket.map(item => (
-                        <CheckoutProduct
-                            id={item.id}
-                            title={item.title}
-                            image={item.image}
-                            price={item.price}
-                            rating={item.rating}
-                        />
-                    ))}
-
-                    {/* CheckoutProduct */}
-                </div>
+                            {basket.map(item => (
+                                <CheckoutProduct
+                                    id={item.id}
+                                    title={item.title}
+                                    image={item.image}
+                                    price={item.price}
+                                    rating={item.rating}
+                                />
+                            ))}
+                            {/* CheckoutProduct */}
+                        </div>
+                    )}
             </div>
             <div className="checkout__right">
                 <Subtotal />
