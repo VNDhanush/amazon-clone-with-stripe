74b45b926e1dffb4fff4a0f976f91af59e0304f2
Dhanush <vndhanush51@gmail.com>
2020-10-08 13:18:00 +0530

    added the hosting thing

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,37 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
-const stripe = require("stripe")('sk_test_51HYp4BBqTmmmhQFGZkXhiOzmvhxBzt3EUfnl0nczs9pMqvf5lJr6EILP8D5rukE9h4spsV767g1Oqs46JkMXQS4V00uLkb1sqU')
+const stripe = require("stripe")("sk_test_51HYp4BBqTmmmhQFGZkXhiOzmvhxBzt3EUfnl0nczs9pMqvf5lJr6EILP8D5rukE9h4spsV767g1Oqs46JkMXQS4V00uLkb1sqU");
 
-//--API
+// API
 
+// - App config
+const app = express();
 
-//--App config
-const app=express();
-
-//--Middlewares
-app.use(cors({origin:true}));
+// - Middlewares
+app.use(cors({ origin: true }));
 app.use(express.json());
 
-//--API routes
-app.get('/',(request,response)=>response.status(200).send('hello world'))
+// - API routes
+app.get("/", (request, response) => response.status(200).send("hello world"));
 
-//--Listen command
-exports.api=functions.https.onRequest(app)
+app.post("/payments/create", async (request, response) => {
+    const total = request.query.total;
+
+    console.log("Payment Request Recieved BOOM!!! for this amount >>> ", total);
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: total, // subunits of the currency
+        currency: "inr",
+    });
+
+    // OK - Created
+    response.status(201).send({
+        clientSecret: paymentIntent.client_secret,
+    });
+});
+
+// - Listen command
+exports.api = functions.https.onRequest(app);
+
+// Example endpoint
